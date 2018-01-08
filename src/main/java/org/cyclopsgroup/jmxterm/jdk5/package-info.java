32cf434df4d70cd02503e60d3be65b9156e2f7d7
Daniel Heid <Daniel.Heid@parshipelite.com>
2018-01-08 20:30:53 +0100

    Fixed javadoc errors

--- src/main/java/org/cyclopsgroup/jmxterm/jdk5/package-info.java
+++ src/main/java/org/cyclopsgroup/jmxterm/jdk5/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Classes to implement {@link JavaProcessManager} in JDK5 specifi way
+ * Classes to implement {@link org.cyclopsgroup.jmxterm.JavaProcessManager} in JDK5 specifi way
  */
 package org.cyclopsgroup.jmxterm.jdk5;
