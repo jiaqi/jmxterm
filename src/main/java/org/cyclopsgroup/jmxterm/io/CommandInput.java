782266a995b621324b229db1bfe272a8f4b1f723
Jiaqi Guo <jguo@chime.com>
2022-07-30 15:07:06 -0700

    Fix

--- src/main/java/org/cyclopsgroup/jmxterm/io/CommandInput.java
+++ src/main/java/org/cyclopsgroup/jmxterm/io/CommandInput.java
@@ -9,12 +9,15 @@ import java.io.IOException;
  * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo</a>
  */
 public abstract class CommandInput implements Closeable {
+  @Override
+  public void close() throws IOException {}
+
   /**
    * Reads a single line from input.
    *
    * @return The line it reads.
    * @throws IOException allows any communication error.
-   **/
+   */
   public abstract String readLine() throws IOException;
 
   /**
@@ -23,6 +26,6 @@ public abstract class CommandInput implements Closeable {
    * @param prompt The full or partial input that user types.
    * @return The string it reads.
    * @throws IOException allows any communication error.
-   **/
+   */
   public abstract String readMaskedString(String prompt) throws IOException;
 }
